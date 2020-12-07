414bfeb5b556ae5c524bc35f4ae6412dfdf7a96d
Mark Olbert <mark@arcabama.com>
2020-12-06 16:27:00 -0800

    adding ability to dedupe coordinates. added additional input file for testing.

--- KMLProcessor/App.cs
+++ KMLProcessor/App.cs
@@ -1,4 +1,6 @@
-﻿using System.IO;
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
@@ -40,7 +42,60 @@ namespace J4JSoftware.KMLProcessor
 
             using var stream = File.OpenText( _config.KmlFile );
             var xDoc = await XDocument.LoadAsync( stream, LoadOptions.None, cancellationToken );
-            var junk = xDoc.Descendants().SingleOrDefault( x => x.Name.LocalName == "coordinates" );
+
+            var rawCoords = xDoc.Descendants()
+                .SingleOrDefault( x => x.Name.LocalName == "coordinates" );
+
+            var coordText = rawCoords!.Value.Replace( "\t", "" )
+                .Replace( "\n", "" );
+
+            var coordinates = coordText.Split( ' ', StringSplitOptions.RemoveEmptyEntries )
+                .Select( t => new Coordinate( t ) )
+                .ToList();
+
+            var minMoveIndex = -1;
+            Coordinate? prior = null;
+            var deDuped = new List<Coordinate>();
+
+            foreach( var coord in coordinates )
+            {
+                if( prior == null )
+                {
+                    prior = coord;
+                    deDuped.Add( coord );
+
+                    continue;
+                }
+
+                if( coord.MinDelta( prior ) == 0.0 )
+                    continue;
+
+                deDuped.Add( coord );
+                prior = coord;
+            }
+
+            var minDelta = double.MaxValue;
+
+            for( var idx = 0; idx < deDuped.Count; idx++ )
+            {
+                var coord = deDuped[ idx ];
+
+                if( prior == null )
+                {
+                    prior = coord;
+                    continue;
+                }
+
+                var delta = coord.MinDelta( prior );
+
+                if( delta < minDelta )
+                {
+                    minDelta = delta;
+                    minMoveIndex = idx;
+                }
+
+                prior = coord;
+            }
 
             _logger.Information( "Done!" );
 
---
+++ KMLProcessor/Coordinate.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace J4JSoftware.KMLProcessor
+{
+    public class Coordinate
+    {
+        public Coordinate()
+        {
+        }
+
+        public Coordinate( string latLong)
+        {
+            var parts = latLong.Split( ',' );
+
+            Latitude = Convert.ToDouble( parts[0] );
+            Longitude = Convert.ToDouble( parts[1] );
+        }
+
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+
+        public double MinDelta( Coordinate prior )
+        {
+            var latDelta = Math.Abs( Latitude - prior.Latitude );
+            var longDelta = Math.Abs( Longitude - prior.Longitude );
+
+            return latDelta < longDelta ? latDelta : longDelta;
+        }
+    }
+}
