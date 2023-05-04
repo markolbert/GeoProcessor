d10e809562e5e8dece779b1246e507e62878b1e7
Mark Olbert <mark@arcabama.com>
2023-05-04 10:06:47 -0700

    update license headers

--- GeoProcessor/support/Coordinate.cs
+++ GeoProcessor/support/Coordinate.cs
@@ -1,20 +1,22 @@
-﻿#region license
-
-// Copyright 2021 Mark A. Olbert
+﻿#region copyright
+// Copyright (c) 2021, 2022, 2023 Mark A. Olbert 
+// https://www.JumpForJoySoftware.com
+// Coordinate.cs
+//
+// This file is part of JumpForJoy Software's GeoProcessor.
 // 
-// This library or program 'GeoProcessor' is free software: you can redistribute it
-// and/or modify it under the terms of the GNU General Public License as
-// published by the Free Software Foundation, either version 3 of the License,
-// or (at your option) any later version.
+// GeoProcessor is free software: you can redistribute it and/or modify it 
+// under the terms of the GNU General Public License as published by the 
+// Free Software Foundation, either version 3 of the License, or 
+// (at your option) any later version.
 // 
-// This library or program is distributed in the hope that it will be useful, but
-// WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-// General Public License for more details.
+// GeoProcessor is distributed in the hope that it will be useful, but 
+// WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY 
+// or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License 
+// for more details.
 // 
-// You should have received a copy of the GNU General Public License along with
-// this library or program.  If not, see <https://www.gnu.org/licenses/>.
-
+// You should have received a copy of the GNU General Public License along 
+// with GeoProcessor. If not, see <https://www.gnu.org/licenses/>.
 #endregion
 
 using System;
