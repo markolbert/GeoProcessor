48286b4d17a03a509bfaa35b2b4ddf949ebc6b39
Mark Olbert <mark@arcabama.com>
2023-05-07 12:37:26 -0700

    convert to class

--- GeoProcessor/revised/data-structs/ExportedRoute.cs
+++ GeoProcessor/revised/data-structs/ExportedRoute.cs
@@ -2,4 +2,25 @@
 
 namespace J4JSoftware.GeoProcessor;
 
-public record ExportedRoute( string FolderName, List<Coordinate2>? Coordinates, SnapProcessStatus Status );
+public class ExportedRoute
+{
+    public ExportedRoute()
+    {
+        Coordinates = new List<Coordinate2>();
+    }
+
+    public ExportedRoute( 
+        string folderName, 
+        List<Coordinate2>? coordinates, 
+        SnapProcessStatus status 
+        )
+    {
+        FolderName = folderName;
+        Coordinates = coordinates;
+        Status = status;
+    }
+
+    public string FolderName { get; set; } = "Unnamed Route";
+    public List<Coordinate2>? Coordinates { get; }
+    public SnapProcessStatus Status { get; set; } = SnapProcessStatus.NoResultsReturned;
+}
--- GeoProcessor/revised/data-structs/ImportedRoute.cs
+++ GeoProcessor/revised/data-structs/ImportedRoute.cs
@@ -2,4 +2,24 @@
 
 namespace J4JSoftware.GeoProcessor;
 
-public record ImportedRoute( string RouteName, List<Coordinate2> Coordinates );
+public class ImportedRoute
+{
+    public ImportedRoute()
+    {
+        Coordinates = new List<Coordinate2>();
+    }
+
+    public ImportedRoute(
+        string routeName,
+        List<Coordinate2> coordinates
+    )
+    {
+        RouteName = routeName;
+        Coordinates = coordinates;
+    }
+
+    public ImportedRoute Copy() => new( RouteName, new List<Coordinate2>( Coordinates ) );
+
+    public string RouteName { get; set; } = "Unnamed Route";
+    public List<Coordinate2> Coordinates { get; }
+}
