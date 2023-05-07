4c59a902937162ca7673c67c9cbe3f500b5eeaf8
Mark Olbert <mark@arcabama.com>
2023-05-07 12:38:39 -0700

    support custom type creation attributes

--- GeoProcessor/revised/factories/NamedTypeFactory.cs
+++ GeoProcessor/revised/factories/NamedTypeFactory.cs
@@ -1,27 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Linq.Expressions;
 using System.Reflection;
 using Microsoft.Extensions.Logging;
 
 namespace J4JSoftware.GeoProcessor;
 
-public abstract class NamedTypeFactory<TCreate, TAttr>
+public abstract class NamedTypeFactory<TCreate>
     where TCreate : class
-    where TAttr : Attribute
 {
-    private readonly Func<TAttr, string> _itemName;
+    private record TypeInfo( Type Type, Attribute Attribute );
+
     private readonly List<Assembly> _assemblies = new();
-    private readonly Dictionary<string, Type> _itemTypes = new( StringComparer.OrdinalIgnoreCase );
+    private readonly Dictionary<string, TypeInfo> _itemTypes = new( StringComparer.OrdinalIgnoreCase );
     private readonly ILogger? _logger;
 
     protected NamedTypeFactory(
-        Expression<Func<TAttr, string>> attrNameBinder,
         ILoggerFactory? loggerFactory = null
     )
     {
-        _itemName = attrNameBinder.Compile();
         _logger = loggerFactory?.CreateLogger( GetType() );
     }
 
@@ -42,16 +39,14 @@ public abstract class NamedTypeFactory<TCreate, TAttr>
                                             .SelectMany( a => a.GetTypes() )
                                             .Where( IncludeType ) )
         {
-            var attr = itemType.GetCustomAttribute<TAttr>();
-            if( attr == null )
+            var attrInfo = GetAttributeInfo( itemType );
+            if( attrInfo == null  )
                 continue;
 
-            var itemName = _itemName( attr );
-
-            if( !_itemTypes.TryAdd( itemName, itemType ) )
+            if( !_itemTypes.TryAdd( attrInfo.Name, new TypeInfo( itemType, attrInfo.Attribute ) ) )
                 _logger?.LogError( "Attempting to add duplicate {type} '{name}' to factory, ignoring",
                                    itemType,
-                                   itemName );
+                                   attrInfo.Name );
         }
 
         return true;
@@ -75,17 +70,22 @@ public abstract class NamedTypeFactory<TCreate, TAttr>
 
     protected abstract bool CheckConstructor( ConstructorInfo ctor );
 
+    protected abstract NamedTypeAttributeInfo? GetAttributeInfo( Type itemType );
+
     public TCreate? this[ string itemName ]
     {
         get
         {
-            if( _itemTypes.TryGetValue( itemName, out var type ) )
-                return CreateInstance( type );
+            if( _itemTypes.TryGetValue( itemName, out var typeInfo ) )
+                return CreateInstance( typeInfo.Type );
 
             _logger?.LogWarning( "Unsupported {type} '{name}'", typeof( TCreate ), itemName );
             return null;
         }
     }
+
+    public Attribute? GetAttribute( string itemName ) =>
+        _itemTypes.TryGetValue( itemName, out var typeInfo ) ? typeInfo.Attribute : null;
 
     protected abstract TCreate? CreateInstance( Type type );
 }
