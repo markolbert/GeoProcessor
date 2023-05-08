12580061260d5440766ed342370044a244aa01c5
Mark Olbert <mark@arcabama.com>
2023-05-08 16:02:53 -0700

    add unit-aware distance object

---
+++ GeoProcessor/revised/measurement/MeasurementSystem.cs
@@ -0,0 +1,7 @@
+﻿namespace J4JSoftware.GeoProcessor;
+
+public enum MeasurementSystem
+{
+    American,
+    Metric
+}
---
+++ GeoProcessor/revised/measurement/UnitType.cs
@@ -0,0 +1,16 @@
+﻿namespace J4JSoftware.GeoProcessor;
+
+public enum UnitType
+{
+    [MeasurementSystem(MeasurementSystem.American, 1)]
+    Feet,
+
+    [MeasurementSystem(MeasurementSystem.Metric, 1)]
+    Meters,
+
+    [MeasurementSystem(MeasurementSystem.American, 5280)]
+    Miles,
+
+    [MeasurementSystem(MeasurementSystem.Metric, 1000)]
+    Kilometers
+}
