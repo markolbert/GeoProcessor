e90d5a3285621faf18380c51b48fbc450f32e475
Mark Olbert <mark@arcabama.com>
2023-05-07 17:43:19 -0700

    remove unneeded code

--- GeoProcessor/revised/importers/Importer.cs
+++ GeoProcessor/revised/importers/Importer.cs
@@ -8,21 +8,12 @@ namespace J4JSoftware.GeoProcessor;
 
 public abstract class Importer : MessageBasedTask, IImporter2
 {
-    private double _maxPtSep = GeoConstants.DefaultMaxPointSeparationKm;
-
     protected Importer(
         string? mesgPrefix = null,
         ILoggerFactory? loggerFactory = null
     )
     : base(mesgPrefix, loggerFactory)
     {
-    }
-
-    // in kilometers
-    public double MaxPointSeparation
-    {
-        get => _maxPtSep;
-        set => _maxPtSep = value <= 0 ? GeoConstants.DefaultMaxPointSeparationKm : value;
     }
 
     public async Task<List<ImportedRoute>> ImportAsync( DataToImportBase toImport, CancellationToken ctx = default )
