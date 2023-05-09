56a334dda6a9b383c7f5bb514ae0bfe1c6b09d1b
Mark Olbert <mark@arcabama.com>
2023-05-09 14:31:42 -0700

    add filters

---
+++ GeoProcessor/revised/filters/RemoveGarminMessagePoints.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Extensions.Logging;
+
+namespace J4JSoftware.GeoProcessor;
+
+[BeforeAllImportFilter("Remove Garmin Message Points", 0)]
+public class RemoveGarminMessagePoints : ImportFilter
+{
+    public RemoveGarminMessagePoints(
+        ILoggerFactory? loggerFactory
+    )
+    :base( loggerFactory)
+    {
+    }
+
+    public override List<IImportedRoute> Filter( List<IImportedRoute> input )
+    {
+        if( input.Any() )
+            return input.Where( route => route.All( x => x.Description == null ) ).ToList();
+
+        Logger?.LogInformation( "Nothing to filter" );
+        return input;
+    }
+}
---
+++ GeoProcessor/revised/filters/RemoveSingelPointRoutes.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Extensions.Logging;
+
+namespace J4JSoftware.GeoProcessor;
+
+[BeforeAllImportFilter("Remove Single Point Routes", 10)]
+public class RemoveSinglePointRoutes : ImportFilter
+{
+    public RemoveSinglePointRoutes(
+        ILoggerFactory? loggerFactory
+    )
+    :base( loggerFactory)
+    {
+    }
+
+    public override List<IImportedRoute> Filter( List<IImportedRoute> input )
+    {
+        if( input.Any() )
+            return input.Where( x => x.NumPoints > 1 ).ToList();
+
+        Logger?.LogInformation( "Nothing to filter" );
+        return input;
+    }
+}
