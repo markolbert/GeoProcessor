c5ce78bc70d3e900da695da165477d5c0cb028b1
Mark Olbert <mark@arcabama.com>
2023-05-12 09:49:51 -0700

    add zipped file exporter

---
+++ GeoProcessor/revised/exporters/KmzExporter2.cs
@@ -0,0 +1,28 @@
+﻿using System.IO;
+using System.IO.Compression;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+
+namespace J4JSoftware.GeoProcessor;
+
+public class KmzExporter2 : KmlExporter2
+{
+    public KmzExporter2(
+        ILoggerFactory? loggerFactory
+    )
+        : base( "kmz", loggerFactory )
+    {
+    }
+
+    protected override async Task OutputMemoryStream( MemoryStream memoryStream )
+    {
+        var entryPath = ChangeFileExtension( FilePath, "kml" );
+
+        await using var zipFile = new FileStream( FilePath, FileMode.Create );
+        using var archive = new ZipArchive( zipFile, ZipArchiveMode.Create, true );
+        var entry = archive.CreateEntry( entryPath );
+        var zipStream = entry.Open();
+        await memoryStream.CopyToAsync( zipStream );
+        zipStream.Close();
+    }
+}
