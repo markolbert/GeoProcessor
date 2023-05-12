808aa04c72ab38aaa006b1e2187ffe36c68ae9d2
Mark Olbert <mark@arcabama.com>
2023-05-12 10:13:33 -0700

    update license headers

--- Test.GeoProcessor/GpxTests.cs
+++ Test.GeoProcessor/GpxTests.cs
@@ -1,4 +1,25 @@
-﻿using System;
+﻿#region copyright
+// Copyright (c) 2021, 2022, 2023 Mark A. Olbert 
+// https://www.JumpForJoySoftware.com
+// GpxTests.cs
+//
+// This file is part of JumpForJoy Software's Test.GeoProcessor.
+// 
+// Test.GeoProcessor is free software: you can redistribute it and/or modify it 
+// under the terms of the GNU General Public License as published by the 
+// Free Software Foundation, either version 3 of the License, or 
+// (at your option) any later version.
+// 
+// Test.GeoProcessor is distributed in the hope that it will be useful, but 
+// WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY 
+// or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License 
+// for more details.
+// 
+// You should have received a copy of the GNU General Public License along 
+// with Test.GeoProcessor. If not, see <https://www.gnu.org/licenses/>.
+#endregion
+
+using System;
 using System.IO;
 using System.Threading.Tasks;
 using FluentAssertions;
@@ -27,6 +48,8 @@ public class GpxTests : TestBase
             File.Delete( gpxExport );
 
         var kmlExport = Path.Combine( Environment.GetFolderPath( Environment.SpecialFolder.Desktop ), "TestKml.kml" );
+        if (File.Exists(kmlExport))
+            File.Delete(kmlExport);
 
         var kmzExport = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), "TestKml.kmz");
         if (File.Exists(kmzExport))
