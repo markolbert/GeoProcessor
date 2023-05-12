d589d595bb7d536adb951a160d3d7cb81bcdbabc
Mark Olbert <mark@arcabama.com>
2023-05-12 09:50:11 -0700

    add default icon/style constants

--- GeoProcessor/constants/GeoConstants.cs
+++ GeoProcessor/constants/GeoConstants.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Drawing;
 
 namespace J4JSoftware.GeoProcessor;
 
@@ -6,4 +7,7 @@ public partial class GeoConstants
 {
     public static TimeSpan DefaultRequestTimeout { get; } = TimeSpan.FromSeconds(20);
     public const int DefaultStatusInterval = 500;
+    public static Color DefaultRouteColor { get; }= Color.Blue;
+    public static int DefaultRouteWidth = 10;
+    public const string DefaultIconSourceHref = "http://maps.google.com/mapfiles/kml/paddle/wht-blank.png";
 }
