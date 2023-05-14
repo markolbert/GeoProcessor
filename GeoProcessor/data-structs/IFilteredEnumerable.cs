03ff1d8aa12adf1240f8c99c85ac876f4a494f72
Mark Olbert <mark@arcabama.com>
2023-05-14 12:48:31 -0700

    define interface for filtered Point collections

---
+++ GeoProcessor/data-structs/IFilteredEnumerable.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace J4JSoftware.GeoProcessor;
+
+public interface IFilteredEnumerable
+{
+    IEnumerable<Point> GetFilteredPoints(Distance? minSeparation = null, Distance? maxOverallGap = null);
+    bool Any(Distance? minSeparation = null, Distance? maxOverallGap = null);
+}
