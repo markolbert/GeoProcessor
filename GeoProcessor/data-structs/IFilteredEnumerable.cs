baa88d4264b50e606df17f03b7340284f215992e
Mark Olbert <mark@arcabama.com>
2023-05-16 11:20:38 -0700

    simplify route/points approach

--- GeoProcessor/data-structs/IFilteredEnumerable.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections.Generic;
-
-namespace J4JSoftware.GeoProcessor;
-
-public interface IFilteredEnumerable
-{
-    IEnumerable<Point> GetFilteredPoints(Distance? minSeparation = null, Distance? maxOverallGap = null);
-    bool Any(Distance? minSeparation = null, Distance? maxOverallGap = null);
-}
