c8d61f6a62948ebb672dbb7e9283f1f27614e13c
Mark Olbert <mark@arcabama.com>
2023-05-17 11:27:51 -0700

    clarify names

--- GeoProcessor/constants/GeoConstants.cs
+++ GeoProcessor/constants/GeoConstants.cs
@@ -27,7 +27,7 @@ namespace J4JSoftware.GeoProcessor;
 public partial class GeoConstants
 {
     public static TimeSpan DefaultRequestTimeout { get; } = TimeSpan.FromSeconds(20);
-    public const int DefaultStatusInterval = 500;
+    public const int DefaultProgressInterval = 500;
     public static Color DefaultRouteColor { get; }= Color.Blue;
     public static int DefaultRouteWidth = 10;
     public const string DefaultIconSourceHref = "http://maps.google.com/mapfiles/kml/paddle/wht-blank.png";
